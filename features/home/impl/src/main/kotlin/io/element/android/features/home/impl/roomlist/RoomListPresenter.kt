5b0ca5c5b8bae789a8875099eb1e66a68b15ab8f
Samuel Truman <mail@samueltruman.com>
2026-01-22 20:34:41 +0100

    Add sessionId to shortcut data

--- features/home/impl/src/main/kotlin/io/element/android/features/home/impl/roomlist/RoomListPresenter.kt
+++ features/home/impl/src/main/kotlin/io/element/android/features/home/impl/roomlist/RoomListPresenter.kt
@@ -115,6 +115,7 @@ class RoomListPresenter(
                     val shortcuts = topRooms.map { summary ->
                         SharingRoomInfo(
                             roomId = summary.roomId.value,
+                            sessionId = client.sessionId.value,
                             displayName = summary.name ?: summary.roomId.value,
                             avatarUrl = summary.avatarData.url
                         )
